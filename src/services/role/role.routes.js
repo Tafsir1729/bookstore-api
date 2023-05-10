fd2c9d266143920456a0f2ff11a26828d30b7c21
Tafsir Ahamed <tafsira@numberskills.se>
2023-05-10 12:36:07 +0600

    changes

--- src/services/role/role.routes.js
+++ src/services/role/role.routes.js
@@ -1,16 +1,30 @@
 const { Router } = require("express");
 const { create, getAll } = require("./role.controller");
 const cors = require("cors");
+const { isAdmin } = require("../../utils/protected");
+
+const serverUrl = "http://localhost:5000";
 
 const router = Router();
+
 router.post(
   "/role/create",
   cors({
-    origin: "http://localhost:5000",
+    origin: serverUrl,
     optionsSuccessStatus: 201,
   }),
+  isAdmin,
   create
 );
-router.get("/role/get", getAll);
+
+router.get(
+  "/role/get",
+  cors({
+    origin: serverUrl,
+    optionsSuccessStatus: 200,
+  }),
+  isAdmin,
+  getAll
+);
 
 module.exports = router;
--- src/services/user/user.routes.js
+++ src/services/user/user.routes.js
@@ -1,25 +1,38 @@
 const { Router } = require("express");
 const cors = require("cors");
-const { register, verifyOTP } = require("./user.controller");
+const { register, verifyOTP, login } = require("./user.controller");
 const { registerValidator } = require("./user.validator");
 
+const serverUrl = "http://localhost:3000";
+
 const router = Router();
+
 router.post(
   "/user/register",
   cors({
-    origin: "http://localhost:3000",
+    origin: serverUrl,
     optionsSuccessStatus: 201,
   }),
   registerValidator,
   register
 );
+
 router.post(
   "/user/verifyOTP",
   cors({
-    origin: "http://localhost:3000",
+    origin: serverUrl,
     optionsSuccessStatus: 200,
   }),
   verifyOTP
+);
+
+router.post(
+  "/user/login",
+  cors({
+    origin: serverUrl,
+    optionsSuccessStatus: 200,
+  }),
+  login
 );
 
 module.exports = router;
